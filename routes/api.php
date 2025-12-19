c467806f1df2a9c7b2be0dd39568bbb2165ae6d9
AkbarSarmast <aksa84ln@gmail.com>
2025-12-19 06:34:55 -0500

    updated some api routes and migrations

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+use App\Http\Controllers\TaskController;
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::get('/tasks/{id}', [TaskController::class, 'show']);
+    Route::put('/tasks/{id}', [TaskController::class, 'update']);
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+});
